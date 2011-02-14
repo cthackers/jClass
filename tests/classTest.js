e137038524f506b1f12673b3d74fd91f53fb9c45
The Brain <iacob.campia@gmail.com>
2011-02-14 13:58:56 +0200

    Implementation of jInterface

--- tests/classTest.js
+++ tests/classTest.js
@@ -1,7 +1,46 @@
 /**
- * Created by .
- * User: nascai
+ * Created by iacob.campia@gmail.com.
  * Date: 2/14/11
  * Time: 1:43 PM
- * To change this template use File | Settings | File Templates.
  */
+
+$(document).ready(function(){
+    module("Core");
+
+    test("Classes", function() {
+
+        var tmp = jClass({
+            constructor : function(init) {
+                console.log("inside constructr", init);
+            },
+            public : {
+                test : function(argument) {
+                    console.log("test method", argument)
+                },
+                val : 2
+            },
+            private : {
+                testPrivate : function(argument) {
+                    console.log("test private", argument)
+                },
+                value : 3
+            },
+            static : {
+                STAT : function(argument) {
+                    console.log("static", argument)
+                },
+                STAT_VAL : 4
+            }
+        });
+
+        ok(true, "class test")
+        console.log("class");
+        console.dir(tmp);
+
+        var x = new tmp();
+
+        console.log("instance");
+        console.dir(x);
+
+    });
+});
--- tests/interfaceTest.js
+++ tests/interfaceTest.js
@@ -10,41 +10,41 @@ $(document).ready(function(){
     test("Interfaces", function() {
 
         try {
-            var fail = jClass.interface({ willFail : function(a) { a = 1; } });
+            jClass.interface({ willFail : function(a) { a = 1; } });
         } catch (ex) {
             equal(ex.message, "Interface must not implement a method body (on method: willFail)",
                     "Cannot define bodies for interface methods");
         }
 
         try {
-            var fail = jClass.interface({ willFail : 1 });
+            jClass.interface({ willFail : 1 });
         } catch (ex) {
-            equal(ex.message, "Interface property `willFail` can't have a value. Define it using types (eg willFail : Number)",
-                    "Cannot define values for interface attributes (Numbers)");
+            equal(ex.message, "Interface property `willFail` can't have a value. Define it using a type instead of a value (eg willFail : Number)",
+                    "Cannot define values for interface properties (Numbers)");
         }
 
         try {
-            var fail = jClass.interface({ willFail : "test" });
+            jClass.interface({ willFail : "test" });
         } catch (ex) {
-            equal(ex.message, "Interface property `willFail` can't have a value. Define it using types (eg willFail : String)",
-                    "Cannot define values for interface attributes (Strings)");
+            equal(ex.message, "Interface property `willFail` can't have a value. Define it using a type instead of a value (eg willFail : String)",
+                    "Cannot define values for interface properties (Strings)");
         }
 
         try {
-            var fail = jClass.interface({ willFail : true });
+            jClass.interface({ willFail : true });
         } catch (ex) {
-            equal(ex.message, "Interface property `willFail` can't have a value. Define it using types (eg willFail : Boolean)",
-                    "Cannot define values for interface attributes (Booleans)");
+            equal(ex.message, "Interface property `willFail` can't have a value. Define it using a type instead of a value (eg willFail : Boolean)",
+                    "Cannot define values for interface properties (Booleans)");
         }
 
         try {
-            var fail = jClass.interface({ willFail : [1,2] });
+            jClass.interface({ willFail : [1,2] });
         } catch (ex) {
-            equal(ex.message, "Interface property `willFail` can't have a value. Define it using types (eg willFail : Object)",
-                    "Cannot define values for interface attributes (Arrays and Objects)");
+            equal(ex.message, "Interface property `willFail` can't have a value. Define it using a type instead of a value (eg willFail : Object)",
+                    "Cannot define values for interface properties (Arrays and Objects)");
         }
 
-        var interface = jClass.interface({
+        var myInterface = jClass.interface({
             test : function(a,b,c) {},
             noArgs : function() {},
             b : Number,
@@ -53,45 +53,12 @@ $(document).ready(function(){
         });
 
         try {
-            interface()
+            myInterface()
         } catch (ex) {
             equal(ex.message, "An interface cannot be instantiated!", "Cannot instantiate interfaces")
         }
-        
-        console.log("interface", typeof interface);
-        console.dir(interface);
 
-        var tmp = jClass({
-            constructor : function(init) {
-                console.log("inside constructr", init);
-            },
-            public : {
-                test : function(argument) {
-                    console.log("test method", argument)
-                },
-                val : 2
-            },
-            private : {
-                testPrivate : function(argument) {
-                    console.log("test private", argument)
-                },
-                value : 3
-            },
-            static : {
-                STAT : function(argument) {
-                    console.log("static", argument)
-                },
-                STAT_VAL : 4
-            }
-        });
-
-        console.log("class");
-        console.dir(tmp);
-
-        var x = new tmp();
-
-        console.log("instance");
-        console.dir(x);
+        equal(myInterface.type, "jInterface", "myInterface.type == 'jInterface'");
         
     });
 });
